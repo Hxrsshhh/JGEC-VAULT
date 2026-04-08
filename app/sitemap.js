5054bc44e9e67baac7bf171766a17eb276742d71
HarshSingh <harshsingharya612@gmail.com>
2026-04-08 20:43:54 +0530

    sitemap.xml is added

---
+++ app/sitemap.js
@@ -0,0 +1,7 @@
+export default function sitemap() {
+  return [
+    { url: "https://jgec-vault.vercel.app" },
+    { url: "https://jgec-vault.vercel.app/signin" },
+    { url: "https://jgec-vault.vercel.app/signup" },
+  ];
+}
